9c7f5f4c41be130f65bc9ae098f5422028c60991
mmr <mmr@gmail.com>
2021-08-06 17:44:13 +0530

    wlcome github

---
+++ Desktop/AAA.sh
@@ -0,0 +1,93 @@
+!DOCTYPE html>
+<html>
+<head>
+	<title> m form </title>
+</head>
+<body>
+     <form>	
+	<table>
+	  <tr>
+		<td>
+			.....................
+		</td>
+		<td>
+			settings.........................
+		</td>
+		
+	  </tr>
+	  <tr>
+		<td>
+		    Birth day:
+		</td>
+		<td>
+		    <input type="Birth day" placeholder="Birth day Date"  name="">
+		</td>
+		<td>
+		    through AI
+		</td>
+	   </tr>
+	   <tr>
+		<td>
+		    Marriage day:
+		</td>
+		<td>
+		    <input type="Marriage day" placeholder="Marriage day Date" name="">
+		</td>
+	   </tr>
+	   <tr>
+		<td>
+		    Death day:
+		</td>
+		<td>
+		    <input type="Death day" placeholder="Death day Date" name="">
+		</td>
+	   </tr>
+	    <tr>
+		<td>
+		    Email:
+		</td>
+		<td>
+		    <input type="Email" placeholder="Email address" name="">
+		</td>
+	   </tr>
+	    <tr>
+		<td>
+		    Gender:
+		</td>
+		<td>
+		    <input type="radio" name="gender" >male
+		    <input type="radio" name="gender" >female
+		</td>
+	   </tr>
+	   <tr>
+		<td>
+		    submit:
+		</td>
+		<td>
+		    <input type="submit" value="submit" name="">
+		</td>
+	   </tr>			
+	</table>
+     </form>
+	 <div id="smallcont">
+		<div></div>
+		<div id="try1">lets play </div>
+	 </div>
+		<div id="container">
+		<div class="square"></div>
+		<div class="square"></div>
+		<div class="square"></div>
+		<div class="square"></div>
+		<div class="square"></div>
+	 </div>
+	 <h1> welcome </h1>
+</body>
+</html>
+
+
+
+
+
+
+
+
